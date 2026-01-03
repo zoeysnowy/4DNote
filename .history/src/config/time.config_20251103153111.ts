103582cd63619a1ef5ba39badafcb0ad9bfbdd47
Zoey <zoeysnowy@github.com>
2026-01-03 15:22:37 +0800

    chore(vscode): drop .history and tune excludes

--- .history/src/config/time.config_20251103153111.ts
+++
@@ -1,8 +0,0 @@
-import { TimePolicy } from '../types/time';
-
-// Default policy aligned with local-time storage and Monday as week start
-export const defaultTimePolicy: TimePolicy = {
-  defaultTimeOfDay: '09:00',
-  weekStart: 1, // Monday
-  windowResolution: 'snap-to-start',
-};
